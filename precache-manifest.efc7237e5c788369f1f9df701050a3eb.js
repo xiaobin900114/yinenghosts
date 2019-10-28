776bfd498b66b17932741d6c3d2c494f8973fb5c
xiaobin900114 <17800697+xiaobin900114@users.noreply.github.com>
2019-10-27 21:56:07 -0400

    Updates

---
+++ precache-manifest.efc7237e5c788369f1f9df701050a3eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "800981856c60a2140194f1ebdee90536",
+    "url": "/yinenghosts/index.html"
+  },
+  {
+    "revision": "089f5a468f2d049dd615",
+    "url": "/yinenghosts/static/css/main.ec379d5d.chunk.css"
+  },
+  {
+    "revision": "805b77f45bb5f10104a7",
+    "url": "/yinenghosts/static/js/2.fd57658f.chunk.js"
+  },
+  {
+    "revision": "089f5a468f2d049dd615",
+    "url": "/yinenghosts/static/js/main.902782fc.chunk.js"
+  },
+  {
+    "revision": "d1b2c1227123fc5b6a19",
+    "url": "/yinenghosts/static/js/runtime-main.1f95f9cb.js"
+  },
+  {
+    "revision": "331b9b2f9c4fbb89db844d1fc3ce4a35",
+    "url": "/yinenghosts/static/media/core77.331b9b2f.png"
+  },
+  {
+    "revision": "f435574460af55799641d4dbd3967ee3",
+    "url": "/yinenghosts/static/media/idea.f4355744.png"
+  },
+  {
+    "revision": "f68ec9a3538214b69cd136e1545311df",
+    "url": "/yinenghosts/static/media/spark.f68ec9a3.png"
+  }
+]);
