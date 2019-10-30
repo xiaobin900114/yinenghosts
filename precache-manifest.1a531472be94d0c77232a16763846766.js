df79b291b503125c2d6f9d7b2876eabab05d4cd5
xiaobin900114 <17800697+xiaobin900114@users.noreply.github.com>
2019-10-29 23:22:44 -0400

    Updates

--- precache-manifest.1a531472be94d0c77232a16763846766.js
+++ precache-manifest.1a531472be94d0c77232a16763846766.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "800981856c60a2140194f1ebdee90536",
+    "revision": "f675343a92adac8f61cd0f4368538842",
     "url": "/yinenghosts/index.html"
   },
   {
-    "revision": "089f5a468f2d049dd615",
-    "url": "/yinenghosts/static/css/main.ec379d5d.chunk.css"
+    "revision": "3583c447a7ca5084d4da",
+    "url": "/yinenghosts/static/css/main.d858555c.chunk.css"
   },
   {
-    "revision": "805b77f45bb5f10104a7",
-    "url": "/yinenghosts/static/js/2.fd57658f.chunk.js"
+    "revision": "dd9671e115408b76cc19",
+    "url": "/yinenghosts/static/js/2.5d3524ce.chunk.js"
   },
   {
-    "revision": "089f5a468f2d049dd615",
-    "url": "/yinenghosts/static/js/main.902782fc.chunk.js"
+    "revision": "3583c447a7ca5084d4da",
+    "url": "/yinenghosts/static/js/main.1a139283.chunk.js"
   },
   {
     "revision": "d1b2c1227123fc5b6a19",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/yinenghosts/precache-manifest.efc7237e5c788369f1f9df701050a3eb.js"
+  "/yinenghosts/precache-manifest.1a531472be94d0c77232a16763846766.js"
 );
 
 self.addEventListener('message', (event) => {
